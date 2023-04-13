d720e9523086f5fe578c3a013662f4111ccfd1f7
Kriss Rauhvargers <kriss.rauhvargers@lu.lv>
2023-04-13 23:30:33 +0300

    Defined routes for collector coins

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('emonetas-welcome');
 });
+
+Route::get('/coin/', function () {
+    return 'TODO: show a list of all coins';
+});
+
+Route::get('/coin/create', function () {
+    return "TODO: return a form where user can enter information about a new coin";
+});
+
+Route::get('/coin/{slug}', function (string $slug) {
+    return "TODO: look up coin by its url slug (" . htmlspecialchars($slug) . ") and display its data";
+});
+
+Route::get('/coin/{slug}/edit', function (string $slug) {
+    return "TODO: look up coin by its url slug (" . htmlspecialchars($slug) . ") and display an edit form of that coin";
+});
+
