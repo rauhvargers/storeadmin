c97317a70c581a1cf3149798591929004d2eb99a
Kriss Rauhvargers <kriss.rauhvargers@lu.lv>
2023-05-12 12:53:35 +0300

    Extending CoinDetail

--- resources/views/emonetas-welcome.blade.php
+++ resources/views/emonetas-welcome.blade.php
@@ -1,9 +1,21 @@
 <x-layout title='e-Monetas.lv admin panel' description='Admin it all!'>
 
 
-    <x-coin-detail id="1" />
-    <x-coin-detail id="2" />
-    <x-coin-detail id="3" />
+    <x-coin-detail id="1">
+        Our New Coin
+    </x-coin-detail>
+
+    <x-coin-detail id="2">Winner of 2022</x-coin-detail>
+
+    <x-coin-detail id="3">
+        Take a <a href="https://www.lu.lv">LU</a>k at this coin!
+
+        <x-slot name="footer">
+            <p>Extra footer information</p>
+        </x-slot>
+
+    </x-coin-detail>
+
     <x-coin-detail id="4" />
 
     <ul>
